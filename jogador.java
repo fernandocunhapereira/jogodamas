34618420919e3efbcac41e4f7c70d59494b58878
fernandocunhapereira <fernandocunhapereira@gmail.com>
2020-06-07 13:46:10 -0300

    adicionado jogador.java, mas arquivo não é utilizado ainda

---
+++ jogador.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package JogoDamas;
+
+/**
+ *
+ * @author Fernando
+ */
+public class jogador {
+    String nome;
+    int partidas;
+    int vitorias;
+    int derrotas;
+}
